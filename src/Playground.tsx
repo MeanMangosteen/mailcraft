d67bfb673224cc098cece358696320eee9b294f7
Aashwin Varshney <aashwin.varshney@gmail.com>
2020-06-22 16:57:14 +1000

    wip. glow follows mouse.

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -26,8 +26,11 @@ export const Playground = () => {
       <LuckyDuckyAura
         onMouseMove={handleMouse}
         onMouseLeave={() => setMousePos(undefined)}
+        mouse={mousePos}
       >
-        <LuckyDucky />
+        <MegaWrapper mouse={mousePos}>
+          <LuckyDucky>Some content</LuckyDucky>
+        </MegaWrapper>
       </LuckyDuckyAura>
     </PlaygroundContianer>
   );
@@ -49,8 +52,10 @@ const LuckyDucky = styled.div`
 `;
 
 const LuckyDuckyAura = styled.div`
-  padding: 8rem;
-  background: linear-gradient(
+  /* padding: 8rem; */
+  background: ${({ mouse }: { mouse: any }) =>
+    mouse
+      ? `linear-gradient(
       90deg,
       rgba(255, 255, 255, 1) 0%,
       rgba(133, 255, 231, 1) 15%,
@@ -65,6 +70,65 @@ const LuckyDuckyAura = styled.div`
       rgba(106, 255, 226, 1) 50%,
       rgba(157, 255, 236, 1) 85%,
       rgba(255, 255, 255, 1) 100%
-    );
+    ), radial-gradient(at ${mouse && mouse.x}% ${
+          mouse && mouse.y
+        }%, rgba(255,133,133,1) 0%, rgba(255,255,255,0) 40%)`
+      : `linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(133, 255, 231, 1) 15%,
+      rgba(106, 255, 226, 1) 50%,
+      rgba(157, 255, 236, 1) 85%,
+      rgba(255, 255, 255, 1) 100%
+    ),
+    linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(133, 255, 231, 1) 15%,
+      rgba(106, 255, 226, 1) 50%,
+      rgba(157, 255, 236, 1) 85%,
+      rgba(255, 255, 255, 1) 100%)`};
+/* position: relative;
+    &::before{
+      content: ""; // ::before and ::after both require content
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  background: ${({ mouse }: { mouse: any }) =>
+    mouse
+      ? `radial-gradient(at ${mouse && mouse.x}% ${
+          mouse && mouse.y
+        }%, rgba(255,133,133,1) 0%, rgba(255,255,255,0) 40%)`
+      : undefined};
+    background-image: linear-gradient(120deg, #eaee44, #33d0ff);
+    opacity: .7;
+    background: rgb(255,255,255);
+background: radial-gradient(circle, rgba(255,255,255,0) 29%, rgba(255,255,255,1) 100%);
+    }
+
   background-blend-mode: screen;
+
+  background: ${({ mouse }: { mouse: any }) =>
+    `radial-gradient(at ${mouse && mouse.x}% ${
+      mouse && mouse.y
+    }%, rgba(255,133,133,1) 0%, rgba(255,255,255,0) 40%)`}; */
+    box-shadow: inset 0px 0px 9px 10px #ffffff;
+`;
+
+const MegaWrapper = styled.div`
+  padding: 8rem;
+  /* background: rgb(255, 255, 255);
+  background: radial-gradient(
+    circle,
+    rgba(255, 255, 255, 0) 29%,
+    rgba(255, 255, 255, 1) 100%
+  ); */
+  background: ${({ mouse }: { mouse: any }) =>
+    mouse
+      ? `radial-gradient(at ${mouse && mouse.x}% ${
+          mouse && mouse.y
+        }%, rgba(255,133,133,1) 0%, rgba(255,255,255,0) 40%)`
+      : undefined};
 `;
